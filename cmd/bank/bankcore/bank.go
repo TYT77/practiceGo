00190370707ebf3e837898d1e2bc1284f02afd20
CCC621 <ec98721zzz@gmail.com>
2023-03-22 22:17:01 +0900

    コア部分に顧客構造体とアカウント構造体を作成

--- cmd/bank/bankcore/bank.go
+++ cmd/bank/bankcore/bank.go
@@ -1,5 +1,19 @@
 package bank
 
+// Customer ...
+type Customer struct {
+    Name    string
+    Address string
+    Phone   string
+}
+
+// Account ...
+type Account struct {
+    Customer
+    Number  int32
+    Balance float64
+}
+
 func Hello() string {
     return "Hey! I'm working!"
 }
