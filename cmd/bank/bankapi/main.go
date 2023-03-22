e5c19bfa1e7bed79cc7c74e83edfab6d7cb6133c
CCC621 <ec98721zzz@gmail.com>
2023-03-22 22:41:12 +0900

    deposit methodの実装とエンドポイントの設定

--- cmd/bank/bankapi/main.go
+++ cmd/bank/bankapi/main.go
@@ -22,7 +22,11 @@ func main() {
         Number: 1001,
     }
 
+    // エンドポイントの設定
     http.HandleFunc("/statement", statement)
+    http.HandleFunc("/deposit", deposit)
+
+    // apiの公開
     log.Fatal(http.ListenAndServe("localhost:8000", nil))
 
 }
@@ -43,6 +47,34 @@ func statement(w http.ResponseWriter, req *http.Request) {
             fmt.Fprintf(w, "Account with number %v can't be found!", number)
         } else {
             fmt.Fprintf(w, account.Statement())
+        }
+    }
+}
+
+func deposit(w http.ResponseWriter, req *http.Request) {
+    numberqs := req.URL.Query().Get("number")
+    amountqs := req.URL.Query().Get("amount")
+
+    if numberqs == "" {
+        fmt.Fprintf(w, "Account number is missing!")
+        return
+    }
+
+    if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+        fmt.Fprintf(w, "Invalid account number!")
+    } else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+        fmt.Fprintf(w, "Invalid amount number!")
+    } else {
+        account, ok := accounts[number]
+        if !ok {
+            fmt.Fprintf(w, "Account with number %v can't be found!", number)
+        } else {
+            err := account.Deposit(amount)
+            if err != nil {
+                fmt.Fprintf(w, "%v", err)
+            } else {
+                fmt.Fprintf(w, account.Statement())
+            }
         }
     }
 }
