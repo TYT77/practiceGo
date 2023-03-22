6665f629cae1f07b65dbb4449a8058b6385f4d8a
CCC621 <ec98721zzz@gmail.com>
2023-03-22 22:28:35 +0900

    Withdraw methodを実装

--- cmd/bank/bankcore/bank.go
+++ cmd/bank/bankcore/bank.go
@@ -28,6 +28,20 @@ func (a *Account) Deposit(amount float64) error {
     return nil
 }
 
+// Withdraw ...
+func (a *Account) Withdraw(amount float64) error {
+    if amount <= 0 {
+        return errors.New("the amount to withdraw should be greater than zero")
+    }
+
+    if a.Balance < amount {
+        return errors.New("the amount to withdraw should be greater than the account's balance")
+    }
+
+    a.Balance -= amount
+    return nil
+}
+
 func Hello() string {
     return "Hey! I'm working!"
 }
